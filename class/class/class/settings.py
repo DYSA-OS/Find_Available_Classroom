861f3e347c7c153e80a1e665c0e2504663bf8f5a
ffe4el <codkan20@gmail.com>
2024-05-18 00:04:20 +0900

    fixed untracked files

--- class/class/class/settings.py
+++ class/class/class/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY
+SECRET_KEY = my_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'class.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES
+DATABASES = my_settings.DATABASES
 
 
 # Password validation
