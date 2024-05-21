d66b5cb9ac0664dd8d70af12bbf63d0655746607
ffe4el <codkan20@gmail.com>
2024-05-21 23:24:29 +0900

    크롤링 전처리하는중

--- class/class/findClass/models.py
+++ class/class/findClass/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Building(models.Model):
+    name = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+class Room(models.Model):
+    building = models.ForeignKey(Building, related_name='rooms', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.building.name} - {self.name}'
+
+class Lecture(models.Model):
+    subject = models.CharField(max_length=255)
+    professor = models.CharField(max_length=255)
+    lecture_type = models.CharField(max_length=50)
+    room = models.ForeignKey(Room, related_name='lectures', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.subject} by {self.professor}'
+
+class LectureSchedule(models.Model):
+    lecture = models.ForeignKey(Lecture, related_name='schedules', on_delete=models.CASCADE)
+    day = models.CharField(max_length=10)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    def __str__(self):
+        return f'{self.lecture.subject} on {self.day} from {self.start_time} to {self.end_time}'
